8886a40bece718ff0f043447ca6bea3a66504676
Nuno Lopes <nuno.lopes@lopezi.com>
2020-05-12 14:27:23 +0100

    Add app-bar
    Introduce an app header for brand display and structured first level navigation.

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Fabblink App"/>
-  </div>
+    <div class="home">
+        <mu-appbar style="width: 100%;">
+            Fabblink
+            <mu-button flat slot="right">LOGIN</mu-button>
+        </mu-appbar>
+
+
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    export default {
+        name: 'Home',
+        components: {
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+        }
+    }
 </script>
+
+<style lang="scss">
+    .mu-appbar-title {
+        text-align: left;
+        font-weight: bold;
+        letter-spacing: -1px;
+    }
+</style>
