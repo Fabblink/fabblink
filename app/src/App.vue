c26b524fcb10f716fe346cf8878d71fe692e8613
Nuno Lopes <nuno.lopes@lopezi.com>
2020-05-18 19:49:08 +0100

    last commit

--- app/src/App.vue
+++ app/src/App.vue
@@ -10,6 +10,7 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  font-weight: bold;
   color: #2c3e50;
   a {
     font-weight: bold;
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,11 +1,19 @@
 <template>
     <div class="home">
-        <mu-appbar style="width: 100%;">
+        <mu-appbar style="width: 100%; margin-bottom: auto;">
             Fabblink
             <mu-button flat slot="right">LOGIN</mu-button>
         </mu-appbar>
+        <main class="content">...</main>
 
-
+        <mu-container class="footer">
+            <mu-bottom-nav>
+                <mu-bottom-nav-item title="Search Model" icon="3d_rotation"></mu-bottom-nav-item>
+                <mu-bottom-nav-item title="Buy 3D Model" icon="account_balance_wallet"></mu-bottom-nav-item>
+                <mu-bottom-nav-item title="License 3D Model" icon="cloud_upload"></mu-bottom-nav-item>
+                <mu-bottom-nav-item title="3D Printer" icon="location_on"></mu-bottom-nav-item>
+            </mu-bottom-nav>
+        </mu-container>
     </div>
 </template>
 
@@ -19,6 +27,17 @@
 </script>
 
 <style lang="scss">
+    .home {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+        .content {
+            flex:1;
+        }
+        .footer {
+
+        }
+    }
     .mu-appbar-title {
         text-align: left;
         font-weight: bold;
